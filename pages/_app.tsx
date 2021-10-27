7b651c13733887247b8da9a5596b2239d3a906d3
lahirudilshan <lahiru4unew4@gmail.com>
2021-10-27 22:29:53 +0530

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.scss'
+import type { AppProps } from 'next/app';
+// import 'tailwindcss/tailwind.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex h-screen w-full justify-center items-center">
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
+
+export default Home
